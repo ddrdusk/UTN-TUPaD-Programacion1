6d623e2522b853070a45c16b896f23d9448819b5
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2025-09-02 20:56:27 -0300

    Subo práctica unidad 1
    I did as instructed and copied the previous codes

---
+++ TP 1.py
@@ -0,0 +1,55 @@
+#1) Crear un programa que imprima por pantalla el mensaje: “Hola Mundo!”. 
+#print("Hello World?")
+'''2) Crear un programa que pida al usuario su nombre e imprima por pantalla un saludo usando 
+el nombre ingresado.'''
+'''nombre = input("Cual es su nombre?")
+print(f"Hola {nombre}!")'''
+"""3) Crear un programa que pida al usuario su nombre, apellido, edad y lugar de residencia e 
+imprima por pantalla una oración con los datos ingresados."""
+"""nombre = input("Cual es su nombre?")
+apellido = input("Cual es su apellido?")
+edad = input("Cual es su edad?")
+lugar = input("Cual es su lugar de residencia?")
+print(f"Hola! yo soy {nombre} {apellido}, tengo {edad} y vivo en {lugar}")"""
+#4) Crear un programa que pida al usuario el radio de un círculo e imprima por pantalla su área y 
+#su perímetro.
+#rad = float(input("Ingrese radio"))
+#area = 3.14*rad*rad
+#perime = 2*3.14*rad
+#print(f"su area es {area} y su perimetro es {perime}")
+'''5) Crear un programa que pida al usuario una cantidad de segundos e imprima por pantalla a 
+cuántas horas equivale.'''
+'''seg = float(input("Ingrese numero de segundos"))
+hora = seg/60
+print(f"pasaron {hora} hora/s")'''
+#6) Crear un programa que pida al usuario un número e imprima por pantalla la tabla de 
+#multiplicar de dicho número.
+#num = int(input("Ingrese un numero"))
+#print(num*1,num*2,num*3,num*4,num*5,num*6,num*7,num*8,num*9,num*10)
+'''7) Crear un programa que pida al usuario dos números enteros distintos del 0 y muestre por 
+pantalla el resultado de sumarlos, dividirlos, multiplicarlos y restarlos. '''
+'''num1 = int(input("Ingrese un numero que no sea 0"))
+num2 = int(input("Ingrese un numero que no sea 0"))
+suma = num1 + num2
+resta = num1 - num2
+div = num1 / num2
+mult = num1 + num2
+print(f"suma: {suma}. resta: {resta}. multiplicacion: {mult}. division: {div}")'''
+#8) Crear un programa que pida al usuario su altura y su peso e imprima por pantalla su índice 
+#de masa corporal.
+#alt = int(input("Ingrese su altura en metros"))
+#peso = int(input("Ingrese su peso en kilos"))
+#masa = peso / (alt*alt)
+#print(f"su indice de masa corporal es: {masa}")
+'''9) Crear un programa que pida al usuario una temperatura en grados Celsius e imprima por 
+pantalla su equivalente en grados Fahrenheit.'''
+'''temp = int(input("Ingrese temperatura en celsius"))
+fahr = (9/5) * temp + 32
+print(f"latemperatura en Fahrenheit es: {fahr}")'''
+#10) Crear un programa que pida al usuario  3 números e imprima por pantalla el promedio de 
+#dichos números.
+num1 = int(input("Ingrese un numero"))
+num2 = int(input("Ingrese un numero"))
+num3 = int(input("Ingrese un numero"))
+prom = (num1 + num2 + num3) / 3
+print(f"el promedio es: {prom}")
