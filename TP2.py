54dee315903837e93e438a58655adbe40be971ac
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2025-09-07 22:05:44 -0300

    aun mas ejercicios
    6 al 10

--- TP2.py
+++ TP2.py
@@ -44,3 +44,105 @@ print("Ha ingresado una contraseña correcta") if passw <= 14 and passw >= 8 els
 #numeros_aleatorios = [random.randint(1, 100) for i in range(50)] 
 #Nota: el bloque de código anterior crea una lista con 50 números entre 1 y 100 elegidos de 
 #forma aleatoria.
+'''import random
+from statistics import mode, median, mean 
+num =  [random.randint(1, 100) for i in range(50)]
+if(mean(num) > median(num) > mode(num)):
+    print("sesgo positivo")
+elif(mean(num) < median(num) < mode(num)):
+    print("sesgo negativo")
+elif(mean(num) == median(num) == mode(num)):
+    print("sin sesgo")
+else:
+    print("???")'''
+#7) Escribir un programa que solicite una frase o palabra al usuario. Si el string ingresado 
+#termina con vocal, añadir un signo de exclamación al final e imprimir el string resultante por 
+#pantalla; en caso contrario, dejar el string tal cual lo ingresó el usuario e imprimirlo por 
+#pantalla. 
+'''stri = input("Ingrese una frase.")
+ult = stri[len(stri) - 1]
+def isvow(letter):
+    voc = {"a","i","u","e","o","A","I","U","E","O"}
+    return letter in voc
+if(isvow(ult)):
+    print(stri + "!")
+else:
+    print(stri)'''
+#8) Escribir un programa que solicite al usuario que ingrese su nombre y el número 1, 2 o 3 
+#dependiendo de la opción que desee: 
+#1. Si quiere su nombre en mayúsculas. Por ejemplo: PEDRO. 
+#2. Si quiere su nombre en minúsculas. Por ejemplo: pedro. 
+#3. Si quiere su nombre con la primera letra mayúscula. Por ejemplo: Pedro. 
+#El programa debe transformar el nombre ingresado de acuerdo a la opción seleccionada por el 
+#usuario e imprimir el resultado por pantalla. Nota: investigue uso de las funciones upper(), 
+#lower() y title() de Python para convertir entre mayúsculas y minúsculas.
+'''nom = input("ingrese su nombre")
+styl = int(input("elija su estilo (1, 2, o 3)"))
+if(styl == 1):
+    print(nom.upper())
+elif(styl == 2):
+    print(nom.lower())
+elif(styl == 3):
+    print(nom.title())
+else:
+    print("ERROR")'''
+#9) Escribir un programa que pida al usuario la magnitud de un terremoto, clasifique la 
+#magnitud en una de las siguientes categorías según la escala de Richter e imprima el resultado 
+#por pantalla: 
+#● Menor que 3: "Muy leve" (imperceptible). 
+#● Mayor o igual que 3  y menor que 4: "Leve" (ligeramente perceptible). 
+#● Mayor o igual que 4  y menor que 5: "Moderado" (sentido por personas, pero 
+#generalmente no causa daños). 
+#● Mayor o igual que 5  y menor que 6: "Fuerte" (puede causar daños en estructuras 
+#débiles). 
+#● Mayor o igual que 6  y menor que 7: "Muy Fuerte" (puede causar daños significativos). 
+#● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala). 
+'''UDontBelongInThisWrld = float(input("Ingrese la magnitud del terremoto. "))
+if(UDontBelongInThisWrld < 3):
+    print("Muy leve")
+elif(UDontBelongInThisWrld >= 3 and UDontBelongInThisWrld < 4):
+    print("Leve")
+elif(UDontBelongInThisWrld >= 4 and UDontBelongInThisWrld < 5):
+    print("Moderado")
+elif(UDontBelongInThisWrld >= 5 and UDontBelongInThisWrld < 6):
+    print("Fuerte")
+elif(UDontBelongInThisWrld >= 6 and UDontBelongInThisWrld < 7):
+    print("Muy Fuerte")
+else:
+    print("Extremo")'''
+#10)Escribir un programa que pregunte al usuario en cuál hemisferio se encuentra (N/S), qué mes 
+#del año es y qué día es. El programa deberá utilizar esa información para imprimir por pantalla 
+#si el usuario se encuentra en otoño, invierno, primavera o verano. 
+'''hemi = input("Ingrese su hemisferio")
+mes = int(input("Ingrese el mes (numero)"))
+dia = int(input("Ingrese el dia (numero)"))
+def nordie(letter):
+    North = {"n","N","north","North","NORTH"}
+    return letter in North
+def suddey(letter):
+    South = {"s","S","south","South","SOUTH"}
+    return letter in South
+if nordie(hemi):
+    if ((mes == 12 and dia >= 21) or mes < 3 or (mes == 3 and dia <= 20)):
+        print("Invierno")
+    elif(6 > mes > 3 or (mes == 3 and dia >= 21) or (mes == 6 and dia <= 20)):
+        print("Primavera")
+    elif(9 > mes > 6 or (mes == 6 and dia >= 21) or (mes == 9 and dia <= 20)):
+        print("Verano")
+    elif(12 > mes > 9 or (mes == 9 and dia >= 21) or (mes == 12 and dia <= 20)):
+        print("Otoño")
+    else:
+        print("Error con el mes o dia")
+elif suddey(hemi):
+    if ((mes == 12 and dia >= 21) or mes < 3 or (mes == 3 and dia <= 20)):
+        print("Verano")
+    elif(6 > mes > 3 or (mes == 3 and dia >= 21) or (mes == 6 and dia <= 20)):
+        print("Otoño")
+    elif(9 > mes > 6 or (mes == 6 and dia >= 21) or (mes == 9 and dia <= 20)):
+        print("Invierno")
+    elif(12 > mes > 9 or (mes == 9 and dia >= 21) or (mes == 12 and dia <= 20)):
+        print("Primavera")
+    else:
+        print("Error con el mes o dia")
+else:
+    print("Error con el hemisferio")'''
