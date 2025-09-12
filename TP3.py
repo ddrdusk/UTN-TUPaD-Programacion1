86baa3c63b34aeb7515a604e7fac0e6e4db5bff8
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2025-09-12 19:06:32 -0300

    un poco mas
    aun queda por hacer

--- TP3.py
+++ TP3.py
@@ -35,3 +35,29 @@ while inp != 0:
 print(res)'''
 #5) Crea un juego en el que el usuario deba adivinar un número aleatorio entre 0 y 9. Al final, el 
 #programa debe mostrar cuántos intentos fueron necesarios para acertar el número.
+'''import random
+num =  random.randint(1, 9)
+guess = int(input("Adivine un numero del 1 al 9~ "))
+inten = 1
+if(guess != num):
+    while guess != num:
+        guess = int(input("Equivocado~ Adivine otra vez~ "))
+        inten = inten + 1
+    print(f"Correcto~ Usted adivino en {inten} intentos~")
+else:
+    print(f"Correcto~ Usted adivino en {inten} intentos~")'''
+#6) Desarrolla un programa que imprima en pantalla todos los números pares comprendidos 
+#entre 0 y 100, en orden decreciente.
+'''for i in range(100,0,-2):
+    print(i)'''
+#7) Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un 
+#número entero positivo indicado por el usuario.
+'''limit = int(input("1n6r3se un num3r0_"))
+res = 0
+for i in range(0,limit):
+    res = res + i
+print(res)'''
+#8) Escribe un programa que permita al usuario ingresar 100 números enteros. Luego, el 
+#programa debe indicar cuántos de estos números son pares, cuántos son impares, cuántos son 
+#negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad 
+#menor, pero debe estar preparado para procesar 100 números con un solo cambio). 
