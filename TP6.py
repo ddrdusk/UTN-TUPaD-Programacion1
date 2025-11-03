2ba7821d1219891afcdaa9791d07e93d5391bcdc
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2025-11-03 19:27:43 -0300

    it's good
    for now...

--- TP6.py
+++ TP6.py
@@ -18,10 +18,53 @@ precios_frutas['Melon'] = 2800'''
 #4) Escribí un programa que permita almacenar y consultar números telefónicos. 
 #• Permití al usuario cargar 5 contactos con su nombre como clave y número como valor. 
 #• Luego, pedí un nombre y mostrale el número asociado, si existe.
-'''direct = []
-for i in range(3):
+'''direct = {}
+for i in range(5):
     contct = input("Ingresa un nombre ")
     num = int(input("Ingresa su numero "))
-    direct.append({contct : num})
-contct = input("Ingresa un nombre ")
-if '''
+    direct[contct] = num
+contct = input("Cual contacto? ")
+if contct in direct:
+    print(direct[contct])
+else:
+    print("error")'''
+#5) Solicita al usuario una frase e imprime: 
+#• Las palabras únicas (usando un set). 
+#• Un diccionario con la cantidad de veces que aparece cada palabra.
+'''fras = input("Ingrese una frase ")
+palab = fras.split()
+palaSet = set(palab)
+palaCont = {}
+for i in palab:
+    if i in palaCont:
+        palaCont[i] += 1
+    else:
+        palaCont[i] = 1
+print(palaSet)
+print(palaCont)'''
+#6) Permití ingresar los nombres de 3 alumnos, y para cada uno una tupla de 3 notas. 
+#Luego, mostrá el promedio de cada alumno.
+'''from statistics import mean
+notas = {}
+for i in range(3):
+    alum = input("Ingresa un nombre ")
+    nota = (int(input("Ingresa la nota ")), int(input("Ingresa la nota ")), int(input("Ingresa la nota ")))
+    notas[alum] = nota
+for i in notas:
+    print(i)
+    promed = mean(notas[i])
+    print(promed)'''
+#7) Dado dos sets de números, representando dos listas de estudiantes que aprobaron Parcial 1 y Parcial 2: 
+#• Mostrá los que aprobaron ambos parciales. • Mostrá los que aprobaron solo uno de los dos. 
+#• Mostrá la lista total de estudiantes que aprobaron al menos un parcial (sin repetir).
+parc1 = (1,3,4,6)
+parc2 = (2,3,5,6)
+ambparc = []
+aprob = set(parc1 + parc2)
+for i in aprob:
+    if i in parc1 and i in parc2:
+        ambparc.append(i)
+    else:
+        pass
+print(aprob)
+print(ambparc)
